7571e9b2dcbd34b17c5fe08a697590086fe78e29
OlesyaTulinova <olesya_tulinova_04@mail.ru>
2020-02-08 13:12:35 +0300

    mvdmfklmdkfmdkm

---
+++ First program.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+import pygame
+import requests
+
+response = None
+cords = input()
+spn = input()
+map_request = "https://static-maps.yandex.ru/1.x/?ll=" + cords + '&spn=' + spn + '&l=map'
+
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+os.remove(map_file)
